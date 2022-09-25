0d04e275c5fa2a838a68d50bac3269afef265a2e
nikolaizombie1 <fabio.matos999@gmail.com>
2022-09-25 10:42:50 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 use std::process::Command;
-use regex::Regex;
-pub struct SinkNames {
-    sink: String,
-    name: String,
-
-}
 fn main() {
     let sinks = Command::new("bash").
     arg("-c").
@@ -23,8 +17,19 @@ fn main() {
         arg("pactl info | awk '/Default Sink: /{print $3}'").
         output().
         expect("Shell process failed").stdout;
-    let default_string = String::from(String::from_utf8_lossy(&default_str));
+    let mut curr_default = String::from(String::from_utf8_lossy(&default_str));
+    curr_default.truncate(curr_default.len()-1);
     let outputs: Vec<String> = outputs.into_iter().filter(|x| !x.is_empty()).clone().collect();
-    println!("{:?} {}",outputs, default_string);
-    let index: usize = 0;
+    let mut index = 0;
+    for (i,sink) in outputs.clone().into_iter().enumerate() {
+        if sink == curr_default {
+            index = i;
+            break;
+            }
+        }
+    index = index+1;
+    index = index%outputs.len();
+    let new_sink = outputs.get(index).unwrap().clone();
+    let pactl_command = "pactl set-default-sink ".to_string() + &new_sink;
+    Command::new("bash").arg("-c").arg(pactl_command).spawn().expect("Error spawning shell");
 }
